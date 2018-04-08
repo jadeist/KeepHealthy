f20eb2deb1ffe5160ae0186a917a86010808c392
jadeist <jadeegalicia@yahoo.com>
2018-04-08 14:49:04 -0500

    pantalla de index

---
+++ KeepHealthyWeb/assets/js/main.js
@@ -0,0 +1,53 @@
+
+(function($) {
+
+	skel
+		.breakpoints({
+			xlarge: '(max-width: 1680px)',
+			large: '(max-width: 1280px)',
+			medium: '(max-width: 980px)',
+			small: '(max-width: 736px)',
+			xsmall: '(max-width: 480px)'
+		});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		//Desactivar animaciones
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+		//Fix- Error del placeholder
+			$('form').placeholder();
+
+		//Elementos importantes
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		// Nav.
+			$('#nav')
+				.append('<a href="#nav" class="close"></a>')
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'right'
+				});
+
+	});
+
+})(jQuery);
